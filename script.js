acf02d4c373c572814829edd14651d84b8e01f41
Harini <hsingavarapu@apple.com>
2021-07-27 13:47:59 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function Add1(){
+    var count= document.querySelector(".top .num1").innerText;
+    count++;
+    document.querySelector(".top .num1").innerText= count;
+}
+
+function Add2(){
+    var count= document.querySelector(".top .num2").innerText;
+    count++;
+    document.querySelector(".top .num2").innerText= count;
+}
+
+function Add3(){
+    var count= document.querySelector(".top .num3").innerText;
+    count++;
+    document.querySelector(".top .num3").innerText= count;
+}
